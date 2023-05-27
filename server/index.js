e0bb613bcdc4ad2c7da3dad1d969755d63db964f
abduselam <111492530+abduselamm@users.noreply.github.com>
2023-05-27 11:23:15 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,8 +10,7 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use("/employees", employeeRouter);
-const CONNECTION_URL =
-  "mongodb+srv://hubo:Java1234@cluster0.apydubp.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL = mongo_credential;
 const PORT = process.env.PORT || 5000;
 
 mongoose
